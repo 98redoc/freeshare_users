5330f37d831fdfc504f2b1454c02ab9454cbe527
Anh Tra <anhttra-mac@MacBook-Pro.local>
2020-06-23 22:12:12 +0700

    update

--- boot.sh
+++ boot.sh
@@ -1,9 +1,4 @@
 #!/bin/sh -e
 
-# start service discovery task in the background
-if [ "$SERVICE_URL" != "" ]; then
-    python -c "from microflack_common.container import register; register()" &
-fi
-
 # run web server
 exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - manage:app
